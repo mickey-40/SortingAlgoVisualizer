b584d768bee269eb5135ca37122b677f9055d505
mickey-40 <mickeyarnold53@yahoo.com>
2024-09-11 19:34:27 -0500

    added screen and retangles to be display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+
 pygame.init()
 
 class DrawInformation:
@@ -9,6 +10,12 @@ class DrawInformation:
   RED = 255, 0, 0
   GREY = 128, 128, 128
   BACKGROUND_COLOR = WHITE
+
+  GRADIENTS = [
+    (128,128,128),
+    (160,160,160),
+    (192,192,192)
+  ]
 
   SIDE_PAD = 100
   TOP_PAD = 50
@@ -29,3 +36,57 @@ class DrawInformation:
     self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
     self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
     self.start_x = self.SIDE_PAD // 2
+
+def draw(draw_info):
+  draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+  draw_list(draw_info)
+  pygame.display.update()
+
+def draw_list(draw_info):
+  lst = draw_info.lst
+
+
+  for i, val in enumerate(lst):
+    x = draw_info.start_x + i * draw_info.block_width
+    y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+
+    color = draw_info.GRADIENTS[i % 3] 
+    pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
+
+
+def generate_starting_list(n, min_val, max_val):
+  lst = []
+
+  for _ in range(n):
+    val = random.randint(min_val, max_val)
+    lst.append(val)
+
+  return lst
+
+def main():
+  run = True
+  clock = pygame.time.Clock()
+
+  n = 50 
+  min_val = 0
+  max_val = 100
+
+  lst = generate_starting_list(n, min_val, max_val)
+  draw_info = DrawInformation(800, 600, lst)
+
+  while run:
+    clock.tick(60)
+
+    draw(draw_info)
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+
+  pygame.quit()
+
+if __name__ == "__main__":
+  main()
+
