98a6604c4ad4705b22f72426f82c29e513646606
mickey-40 <mickeyarnold53@yahoo.com>
2024-09-13 16:34:46 -0500

    adding title and if ascending or decending on the screen

--- main.py
+++ main.py
@@ -41,14 +41,17 @@ class DrawInformation:
         self.block_height = math.floor((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
         self.start_x = self.SIDE_PAD // 2
 
-def draw(draw_info):
+def draw(draw_info, algo_name, ascending):
     draw_info.window.fill(draw_info.BACKGROUND_COLOR)
 
+    title = draw_info.LARGE_FONT.render(f"{algo_name} - {'Ascending' if  ascending else 'Descending'}", 1, draw_info.GREEN)
+    draw_info.window.blit(title, (draw_info.width/2 - title.get_width()/2, 5))
+
     controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending ", 1, draw_info.BLACK)
-    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
+    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 45))
 
     sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort ", 1, draw_info.BLACK)
-    draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2, 35))
+    draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2, 80))
 
     draw_list(draw_info)
     pygame.display.update()
@@ -123,7 +126,7 @@ def main():
             except StopIteration:
                 sorting = False
         else:
-            draw(draw_info)
+            draw(draw_info, sorting_algo_name, ascending)
 
         pygame.display.update()
 
